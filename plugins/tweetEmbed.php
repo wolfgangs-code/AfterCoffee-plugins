c3a89c563bdcab323d089d9cdfa97be1d13b2d88
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-03-26 15:54:44 -0500

    Plugin: Tweet Embed

---
+++ plugins/tweetEmbed.php
@@ -0,0 +1,25 @@
+<?php
+	#===========================================================#
+	# This plugin for AfterCoffee replaces Twitter URL Links    #
+	# with full HTML embeds.                                    #
+	#===========================================================#
+class tweetEmbed {
+	function addHead() {
+		print("<meta name=\"twitter:widgets:border-color\" content=".USERSET["themeColor"].">");
+	}
+	
+	function changeText($html) {
+		# Tweer URI example: https://twitter.com/BombosArt/status/1367576818958614530
+		$html = preg_replace("/<a href=\"https?:\/\/twitter\.com\/([\w_]{1,15})\/status\/(\d*)\">.*<\/a>/",
+		"<blockquote class=\"twitter-tweet\">
+			<a href=\"https://twitter.com/$1/status/$2\"></a>
+		</blockquote>",
+		$html);
+		return $html;
+	}
+	
+	function addFooter() {
+		print("<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>");
+	}
+}
+?>
