0c37f23daca3272716b9127ba3cb638acf2e82d9
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-03-26 15:56:09 -0500

    Plugin: vanilla-tilt

---
+++ plugins/vanillaTilt.php
@@ -0,0 +1,11 @@
+<?php
+class vanillaTilt {
+	function changeText($html) {
+		$html = preg_replace("/(\[card\])(.*?)(\[\/card\])/", "<div style=\"display: inline-block\"data-tilt data-tilt-glare data-tilt-max-glare=\"0.8\" data-tilt-scale=\"1.1\">$2</div>", $html);
+		return $html;
+	}
+	function addFooter() {
+		print "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js\" integrity=\"sha512-SttpKhJqONuBVxbRcuH0wezjuX+BoFoli0yPsnrAADcHsQMW8rkR84ItFHGIkPvhnlRnE2FaifDOUw+EltbuHg==\" crossorigin=\"anonymous\"></script>\n";
+	}
+}
+?>
