cc027b898ad174d7fc0f70edf768a9dac4f28614
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-07-03 10:25:47 -0500

    vanillaTilt scripts will not load if page has no card

--- plugins/vanillaTilt.php
+++ plugins/vanillaTilt.php
@@ -5,8 +5,9 @@ class vanillaTilt {
 		$html = preg_replace("/(\[card\])(.*?)(\[\/card\])/", "<div style=\"display: inline-block\"data-tilt data-tilt-glare data-tilt-max-glare=\"0.8\" data-tilt-scale=\"1.1\">$2</div>", $html);
 		return $html;
 	}
-	function addFooter() {
-		print "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js\" integrity=\"sha512-SttpKhJqONuBVxbRcuH0wezjuX+BoFoli0yPsnrAADcHsQMW8rkR84ItFHGIkPvhnlRnE2FaifDOUw+EltbuHg==\" crossorigin=\"anonymous\"></script>\n";
+	function addFooter($html) {
+		$hasCard = strpos($html, "data-tilt");
+		!$hasCard ?: print "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js\" integrity=\"sha512-SttpKhJqONuBVxbRcuH0wezjuX+BoFoli0yPsnrAADcHsQMW8rkR84ItFHGIkPvhnlRnE2FaifDOUw+EltbuHg==\" crossorigin=\"anonymous\"></script>\n";
 	}
 	function editorGuide() {
 		$guide = "<b>vanilla-tilt:</b> [card]image.png[/card]";
